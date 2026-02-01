7761f52f9baa6ad02678910f5f1ff2c1f2c0a9be
Salman Raj Chandole <salmanrajchandole@gmail.com>
2026-02-01 10:57:02 +0530

    shell

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 
 echo "script executed at: $TIMESTAMP"
+
+SLEEP 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Scrtipt executed in: $TOTAL_TIME"
