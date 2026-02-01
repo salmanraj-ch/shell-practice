fef7b94f19553fe501bca7f3c5822ff0f6e3c9ac
Salman Raj Chandole <salmanrajchandole@gmail.com>
2026-02-01 22:16:50 +0530

    shell

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,3 +10,11 @@ fi
 echo "Installing Nginx"
 dnf install nginx -y
 
+if [ $? -ne 0 ]; then
+    echo "Installing Nginx ... FAILURE"
+    exit 1
+else
+    echo "Installing Nginx ... SUCCESS"
+fi
+
+
