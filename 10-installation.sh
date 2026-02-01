a03ff16cbdaea9c4237bf416c7e8766036a644f0
Salman Raj Chandole <salmanrajchandole@gmail.com>
2026-02-01 22:11:02 +0530

    shell

--- 10-installation.sh
+++ 10-installation.sh
@@ -3,7 +3,8 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user acess"
+    echo "Please run this script with root user access"
+    exit 1
 fi
 
 echo "Installing Nginx"
