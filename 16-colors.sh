ca42bc6788fdec166632261bea627315da7259b5
Salman Raj Chandole <salmanrajchandole@gmail.com>
2026-02-07 07:23:25 +0530

    trap

--- 16-colors.sh
+++ 16-colors.sh
@@ -3,7 +3,6 @@
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-B="\e[34m"
 N="\e[0m"
 
 echo -e "$R Hello World$N,$Y I am learning Linux $N"
--- 20-trap.sh
+++ 20-trap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
+
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
@@ -15,22 +18,12 @@ fi
 
 mkdir -p $LOGS_FOLDER
 
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
-    fi
-}
-
 for package in $@   #sudo sh 14-loops.sh nginx mysql nodejs
 do
     dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
         echo "$package not installed, installing now"
         dnf install $package -y &>> $LOGS_FILE
-        VALIDATE $? "$package installation"
     else
         echo -e "$package already installed ... $Y skipping $N"
     fi
